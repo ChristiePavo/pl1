f912474879a85c261c105b5290c33d191adc592c
ChristiePavo <christie.peacock@flemingcollege.ca>
2022-01-30 06:43:06 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -3,7 +3,7 @@
 function initMap() {
   const myLatlng = { lat: 44.07588714149805, lng: -78.5025782239259 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
+    zoom: 12,
     center: myLatlng ,
   });
   const marker = new google.maps.Marker({
